1adf0de522e92ce2de3208c91c94515d7a21aaf2
raficx <arminek82@yahoo.com>
2023-03-27 22:10:08 +0200

    zadanie, dzień 4

--- ChalengeApp/ChalengeApp/Program.cs
+++ ChalengeApp/ChalengeApp/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! Happy every day!");
+﻿using System.ComponentModel.Design;
+string imie = "Piotr";
+string sex = "Mężczyzna";
+bool pl = false;
+var wiek = 16;
+//sprawdzenie płci
+if (sex == "Kobieta")
+{
+    pl = true;
+    //sprawdzenie wieku
+    if (wiek < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else
+    {
+        if (imie == "Ewa" && wiek == 33)
+        {
+            Console.WriteLine("Ewa, lat 33");
+        }
+        else
+        {
+            Console.WriteLine("Kobieta, powyżej 29 lat");
+        }
+    }
+
+}
+else
+{
+    pl = false;
+    if (wiek < 18)
+    {
+        Console.WriteLine("Niepełnoletni Mężczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Mężczyzna, powyżej 17 lat");
+    }
+}
+
